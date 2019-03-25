8b0e508d6359a7a50250eb2eb85210f0c1598fa6
Erich Kumpunen <erichtheviolin@gmail.com>
2019-03-25 13:38:25 -0600

    Initial commit

---
+++ Journal-C/Model Controller/EkkEntryController.h
@@ -0,0 +1,17 @@
+//
+//  EkkEntryController.h
+//  Journal-C
+//
+//  Created by Brooke Kumpunen on 3/25/19.
+//  Copyright © 2019 Rund LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EkkEntryController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Journal-C/Model/EKKEntry.m
+++ Journal-C/Model/EKKEntry.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  EKKEntry.m
 //  Journal-C
 //
 //  Created by Brooke Kumpunen on 3/25/19.
 //  Copyright © 2019 Rund LLC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "EKKEntry.h"
 
-@interface ViewController : UIViewController
-
+@implementation EKKEntry
 
 @end
-
